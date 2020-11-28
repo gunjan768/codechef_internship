ed1b80f85538bde8604b4409a6bdfce7f0b44248
Gunjan <57636775+gunjan768@users.noreply.github.com>
2020-11-28 15:12:38 +0530

    updated routes

--- Server/index.php
+++ Server/index.php
@@ -118,95 +118,278 @@
 	    $response->getBody()->write(json_encode($tokenData));
 	    return $response->withAddedHeader('Content-Type', 'application/json');
 	});
-
-	$app->get('/contests[/{code}]', function (Request $request, Response $response, $args) use ($repo) 
+	
+	$RequireAuth = function (Request $request, RequestHandler $handler) 
 	{
-	    $token = Auth::getAccessToken();
+		$response = $handler->handle($request);
+		$response->getBody()->write('World');
 
-	    if(empty($args)) 
-	    {
-	        $response->getBody()->write($repo->getContests($token));
-	    } 
-	    else 
-	    {
-	        $code = $args['code'];
-	        $response->getBody()->write($repo->getContestDetails($token, $code));
-	    }
+		return $response;
+	};
 
-	    return $response->withAddedHeader('Content-Type', 'application/json');
+	$app->post('/api/tags/',function(), use($app)
+	{
+		$user_id;
+
+		$db = new dbHandler();
+
+		$problemId = $app->request()->post('problemId');
+		$tagName = $app->request()->post('tagName');
+
+		try 
+		{
+			$foundActualTag = $db->tags->findOne([ 'tag'=> $tagName ]);
+
+			$updateUserTags = null;
+
+			if(!$foundActualTag) 
+			{
+				$updateUserTags = $db->users->findOneAndUpdate(
+					[ '_id'=> $user_id ],
+					[
+						'$addToSet'=> 
+						[
+							'tags'=> $tagName,
+						]
+					]
+				);
+			}
+
+			$problemTags = $db->problems->findById($problemId, [ '_id'=> 0, 'tags'=> 1 ]);
+
+			$updateProblem = $db->problems->findOneAndUpdate(
+				[ '_id'=> $problemId, "userDefinedTags.user_id"=> [ '$eq'=> $user_id ] ],
+				[
+					'$addToSet'=> 
+					[
+					"userDefinedTags.$.tags"=> $tagName,
+					],
+				]
+			);
+
+			if($updateProblem == null) 
+			{
+				$tagArr=$problemTags->tags;
+				$tagArr->push($tagName);
+
+				$updateProblem = $db->problems->findByIdAndUpdate(
+					$problemId,
+					[
+						'$push'=> 
+						[
+							'userDefinedTags'=> 
+							[
+								'user_id'=> $user_id,
+								'tags'=> $tagArr,
+							],
+						],
+					],
+					[ 'userDefinedTags'=> 1, '_id'=> 0 ]
+				);
+			}
+
+			$updateProblem = $db->problems->findOne(
+				[ '_id'=> $problemId, "userDefinedTags.user_id"=> [ '$eq'=> $user_id ] ],
+				[ '_id'=> 0, 'userDefinedTags'=> 1 ]
+			);
+
+			response(200,
+			[
+				$updateUserTags,
+				$updateProblem,
+			]);
+		} 
+		catch(MongoCursorException $err) 
+		{
+			response(400);
+		}
+	})->add($RequireAuth);
+
+	$app->post('/api/tags/search_tag',function(), use($app)
+	{
+		$value = $app->request()->post('value');
+		$user_id = $app->request()->post('user_id');
+		$db = new dbHandler();
+
+		try 
+		{
+			$data = $db->tags.find(
+				[
+					'tag'=> [ '$regex'=> $value, '$options'=> "i" ],
+				],
+				[
+					'_id'=> 0,
+					'tag'=> 1,
+				]
+			).limit(10);
+			
+
+			if($user_id != null) 
+			{
+				$userData = $db->users->aggregate(
+				[
+					[ '$unwind'=> [ 'path'=> "tags" ] ],
+					[
+						'$match'=> 
+						[ 
+							'$and'=> [[ '_id'=> $user_id ], [ 'tags'=> [ '$regex'=> $value ]]],
+						],
+					],
+					[ '$group'=> [ '_id'=> [ 'tags'=>"tags" ]] ],
+				]).limit(10);
+			}
+
+			return response(200,[ $data, $userData ]);
+		} 
+		catch(MongoCursorException $error) 
+		{
+			return respnse(500);
+		}
 	});
 
-	$app->get('/submissions', function (Request $request, Response $response, $args) use ($repo) 
+	$app->get('/api/tags/tags/{tagType}/{offset}',function()use($app)
 	{
-	    $token = Auth::getAccessToken();
-	    $params = $request->getQueryParams();
+		$tagType = $app->getArgument('tagType');
+		$offset = $app->getArgument('offset')*10;
+		$db = new dbHandler();
 
-	    if(empty($params['contestCode'])) 
-	    {
-	        throw new CustomException("Contest Code is mandatory");
-	    } 
-	    else 
-	    {
-	        if(!empty($params['problemCode'])) 
-	        {
-	            $response->getBody()->write($repo->getProblemSubmissions($token, $params['contestCode'], $params['problemCode']));
-	        } 
-	        else 
-	        {
-	            $response->getBody()->write($repo->getContestSubmissions($token, $params['contestCode']));
-	        }
-	    }
+		try 
+		{
+			$resp =  $db->tag->find([ 'type'=> $tagType ], [ 'tag'=> 1, '_id'=> 0 ])
+			.limit(10)
+			.skip($offset);
 
-	    return $response;
+			if($resp) 
+			{
+				response(400,
+				[
+					$data=> "No tags found",
+				]);
+			} 
+			else 
+			{
+				response(200,$resp);
+			}
+		} 
+		catch(MongoCursorException $err) 
+		{
+			response(400);
+		}
 	});
 
-	$app->get('/rankings', function (Request $request, Response $response) use ($repo) 
+	$app->post('/api/problem/{offset}', function() use($app)
 	{
-	    $token = Auth::getAccessToken();
-	    $params = $request->getQueryParams();
+		$offset = $app->getArgument('offset') * 20;
+		$user_id = $app->request()->post('user_id');
+		$tags = $app->request()->post('tags');
 
-	    if(empty($params['contestCode'])) 
-	    {
-	        throw new CustomException("Contest Code is mandatory");
-	    }
+		$db = new dbHandler();
 
-	    $response->getBody()->write($repo->getContestRankings($token, $params['contestCode']));
+		if($user_id === null) 
+		{
+			try 
+			{
+				$questions = $db->problems->find(
+					[
+						'tags'=> [ '$all'=> $tags ],
+					],
+					[ 'userDefinedTags'=> 0 ]
+				)
+				.limit(20)
+				.skip($offset);
 
-	    return $response;
+				response(200,$questions);
+			} 
+			catch(MongoCursorException $err) 
+			{
+				response(400,$err);
+			}
+		} 
+		else 
+		{
+			try 
+			{
+				$questions = $db->problems->find(
+				[
+					'$or'=> 
+					[
+						[ 'tags'=> [ '$all'=> $tags] ],
+						[
+							'$and'=> 
+							[
+								[ "userDefinedTags.tags"=> [ '$all'=> $tags ] ],
+								[ "userDefinedTags.user_id"=> ($user_id) ],
+							],
+						],
+					],
+				])
+				.limit(20)
+				.skip($offset);
+
+				response(200,$questions);
+			}
+			catch(MongoCursorException $err) 
+			{
+				response(500,[ $err=> "Error fetching problems" ]);
+			}
+		}
 	});
 
-	$app->get('/problem', function (Request $request, Response $response) use ($repo) 
+	$app->post('/api/problem/problem/{problemId}', function() use($app)
 	{
-	    $token = Auth::getAccessToken();
-	    $params = $request->getQueryParams();
+		$res = array();
+		$problemId = $app->getArgument('problemId');
+		$name = $app->request()->post('name');
+		$age = $app->request()->post('age');
 
-	    if(empty($params['contestCode']) || empty($params['problemCode'])) 
-	    {
-	        throw new CustomException("Contest Code & Problem Code is mandatory");
-	    }
+		$db = new dbHandler();
+		$cur = $db->insertFriend($name,$age);
 
-	    $response->getBody()->write($repo->getProblemDetails($token, $params['contestCode'], $params['problemCode']));
+		$user_id = $app->request().post('name');
 
-	    return $response;
+		if($user_id === null) 
+		{
+			$tagsObj = $db->problems->findOne(
+				[ '_id' => $problemId ],
+				[ '_id'=> 0, 'tags'=> 1 ]
+			);
+
+			response(200,[ 'tags'=> $tagsObj->tags ]);
+		} 
+		else 
+		{
+			$tagsObj = $db->problems.findOne(
+				[
+					'_id'=> $problemId,
+					"userDefinedTags->user_id"=> [ '$eq'=> $user_id ],
+				],
+				[ "userDefinedTags.$"=> 1, '_id'=> 0 ]
+			);
+
+			if($tagsObj) 
+			{
+				response(200, ['tags'=> $tagsObj->userDefinedTags[0]->tags ]);
+			} 
+			else 
+			{
+				$tagsObj = $db->problems.findOne(
+					[ '_id'=> $problemId] ,
+					[ '_id'=> 0, 'tags'=> 1 ]
+				);
+
+				response(200,[ 'tags'=> $tagsObj->tags ]);
+			}}
 	});
 
-	$app->post('/ide/run', function (Request $request, Response $response) use ($repo) 
+
+	function response($status, $response) 
 	{
-	    $token = Auth::getAccessToken();
-	    $body = json_decode($request->getBody()->getContents(), true);
-	    $response->getBody()->write($repo->runOnIDE($token, $body['code'], $body['input'], $body['lang']));
+		$app = \Slim\Slim::getInstance();
 
-	    return $response;
-	});
-
-	$app->get('/ide/status', function (Request $request, Response $response) use ($repo) 
-	{
-	    $token = Auth::getAccessToken();
-	    $params = $request->getQueryParams();
-	    $response->getBody()->write($repo->getIDEStatus($token, $params['link']));
-
-	    return $response;
-	});
+		$app->status($status);
+		$app->contentType('application/json');
+		echo json_encode($response, JSON_PRETTY_PRINT);
+	}
 
 	$customErrorHandler = function (
 	    ServerRequestInterface $request,
